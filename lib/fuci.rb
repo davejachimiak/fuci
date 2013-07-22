3e101c12faf1a08f69c9e864f13c003d75398a27
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-07-21 22:47:54 -0400

    Include Fuci.server as an attr_accessor

--- lib/fuci.rb
+++ lib/fuci.rb
@@ -1,10 +1,18 @@
 module Fuci
+  class << self
+    attr_accessor :server
+  end
+
   def self.run
     ensure_server
+    server.fetch_log
   end
 
   private
 
   def self.ensure_server
+    raise Fuci::ServerError, 'A server must be attached to Fuci' unless server
   end
 end
+
+class Fuci::ServerError < StandardError; end;
--- spec/lib/fuci_spec.rb
+++ spec/lib/fuci_spec.rb
@@ -4,13 +4,22 @@ require_relative '../../lib/fuci'
 
 describe Fuci do
   describe '#run' do
+    before do
+      @server     = mock
+      Fuci.server = @server
+
+      @server.expects :fetch_log
+    end
+
     it 'ensures a server is present' do
       Fuci.expects :ensure_server
-
       Fuci.run
     end
 
-    it 'grabs the log from the server'
+    it 'grabs the log from the server' do
+      Fuci.run
+    end
+
     it 'plugs in the test frameworks'
     it 'detects the first framework in the log that has a failure'
     it 'parses the log for and collects failures'
@@ -18,10 +27,19 @@ describe Fuci do
   end
 
   describe '#ensure_server' do
+    before { Fuci.stubs(:server).returns :AnServer }
+
     describe 'when a server is attached to the module' do
       it 'is a no-op' do
-        Fuci.stubs(:server).returns :AnServer
         expect(Fuci.send :ensure_server ).to_equal nil
+      end
+    end
+
+    describe 'when a server is not attached' do
+      before { Fuci.stubs(:server).returns nil }
+
+      it 'raises an error with an appropriate message' do
+        expect { Fuci.send :ensure_server }.to_raise Fuci::ServerError
       end
     end
   end
