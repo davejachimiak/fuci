71007198fda1076c55a55707a79b6c7c9f799bcd
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-07-22 16:29:41 -0400

    Refactor a bit

--- lib/fuci/rspec.rb
+++ lib/fuci/rspec.rb
@@ -1,14 +1,24 @@
 module Fuci
   class RSpec < Fuci::Tester
     FAILURE_INDICATOR = 'Failed examples:'
+    BASE_COMMAND      = 'rspec'
 
     def indicates_failure? log
       log.include? FAILURE_INDICATOR
     end
 
     def command log
-      failures = log.scan(/rspec (.*) #/).join ' '
-      "rspec #{failures}"
+      "#{base_command} #{failures log }"
+    end
+
+    private
+
+    def base_command
+      BASE_COMMAND
+    end
+
+    def failures log
+      log.scan(/rspec (.*) #/).join ' '
     end
   end
 end
