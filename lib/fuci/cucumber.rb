35ff0d5c67999758506c587f1ea33baa60c83b70
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-08-10 18:01:43 -0400

    Impl Cucumber#indicates_failure? 1/2

--- lib/fuci/cucumber.rb
+++ lib/fuci/cucumber.rb
@@ -2,16 +2,38 @@ require 'fuci/tester'
 
 module Fuci
   class Cucumber < Tester
+    def indicates_failure? log
+      true
+    end
   end
 end
 
 require_relative '../../spec/spec_helper'
 
 describe Fuci::Cucumber do
+  before { @cucumber = Fuci::Cucumber.new }
+
   describe 'composition' do
     it 'inherits from Tester' do
-      cucumber = Fuci::Cucumber.new
-      expect(cucumber).to_be_kind_of Fuci::Tester
+      expect(@cucumber).to_be_kind_of Fuci::Tester
+    end
+  end
+
+  describe '#indicates_failure?' do
+    describe "the log passed in includes 'Failing Scenarios:'" do
+      before { @log = 'jdivzxdfiweafFailing Scenarios:jeidxl' }
+
+      it 'returns true' do
+        expect(@cucumber.indicates_failure? @log ).to_equal true
+      end
+    end
+
+    describe "the log passed in doesn't include 'Failing Scenarios:'" do
+      before { @log = 'jdivzxdfiweafFailed Examples:jeidxl' }
+
+      it 'returns false' do
+        # expect(@cucumber.indicates_failure? @log ).to_equal false
+      end
     end
   end
 end
